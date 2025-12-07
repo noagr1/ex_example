d73fb4ddd2715394931f7c8cc8d119cacf2c09c4
noagr1 <noahgrad@gmail.com>
2025-12-07 19:58:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let p1 = document.getElementById("first");
+
+let p11 = document.getElementsByClassName("special")[0];
+
+let p111 = document.querySelector("p");
+
+let p1111 = document.querySelectorAll("p")[0];
